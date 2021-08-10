d3f38d3e287ec3c90474f2e71eaf85568f335a7d
Matt <matheuscardoso1@poli.ufrj.br>
2021-08-10 01:05:10 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'; // , ImageBackground
+// const image = { uri: './assets/app/rio.jpg'}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <ImageBackground source={image} resizeMode="cover" style={styles.background}>
+    // </ImageBackground>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello, I'm <Text style={{color: 'green'}}>Informed</Text></Text>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#cccccc',
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    height: 200
   },
+  text:{
+    fontSize: 30
+  }
 });

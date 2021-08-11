1217393ba5ef8ab87686335283c8e27ee196018c
Matt <matheuscardoso1@poli.ufrj.br>
2021-08-10 23:36:22 -0300

    added icon, splash screen and the background image for the main screen

--- App.tsx
+++ App.tsx
@@ -1,27 +1,49 @@
-// import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'; // , ImageBackground
-// const image = { uri: './assets/app/rio.jpg'}
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+
+const logo = require("./assets/app/logo_transparent.png");
 
 export default function App() {
   return (
-    // <ImageBackground source={image} resizeMode="cover" style={styles.background}>
-    // </ImageBackground>
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, I'm <Text style={{color: 'green'}}>Informed</Text></Text>
-      </View>
+    <>
+      <ImageBackground
+        source={require("./assets/app/rio.jpg")}
+        resizeMode="cover"
+        style={styles.background}
+      >
+        <View style={styles.container}>
+          <Image
+            source={logo}
+            style={{
+              width: 105,
+              height: 160,
+              marginTop: 100,
+            }}
+          />
+        </View>
+      </ImageBackground>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#cccccc',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    height: 200
+    backgroundColor: "#f3f4f1",
+    opacity: 0.2,
+    alignItems: "center",
+    height: 470,
+    width: "95%",
+    marginTop: "20%",
+    borderRadius: 20,
   },
-  text:{
-    fontSize: 30  
-  }
+  logo: {
+    height: 100,
+    width: 100,
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+  },
 });

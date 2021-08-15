4cc33b9aac94c38b575afa8c6fd8102eee23f827
Matt <matheuscardoso1@poli.ufrj.br>
2021-08-15 19:08:49 -0300

    made a Typography component for centralizing anything related to fonts and sizes in text

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+import { StyleSheet, View, ImageBackground, Image, Text } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { Typography, myFonts } from "./src/components";
 
 const logo = require("./assets/app/logo_transparent.png");
 
 export default function App() {
+  let [fontsLoaded] = useFonts(myFonts);
+
+  if (!fontsLoaded) return <AppLoading />;
   return (
     <>
       <ImageBackground
@@ -12,14 +18,30 @@ export default function App() {
         style={styles.background}
       >
         <View style={styles.container}>
-          <Image
-            source={logo}
-            style={{
-              width: 105,
-              height: 160,
-              marginTop: 100,
-            }}
-          />
+          <Text> Texto de controle na fonte padrao.</Text>
+          <Typography fontFamily="rajdhani" type={1}>
+            Rajdhani 1
+          </Typography>
+
+          <Typography fontFamily="rajdhani" type={2}>
+            Radjdhani 2
+          </Typography>
+
+          <Typography fontFamily="rajdhani" type={3}>
+            Rajdhani 3
+          </Typography>
+
+          <Image source={logo} style={styles.logo} />
+
+          <Typography fontFamily="noto-sans" type={1}>
+            Noto-Sans 1
+          </Typography>
+          <Typography fontFamily="noto-sans" type={2}>
+            Noto-Sans 2
+          </Typography>
+          <Typography fontFamily="noto-sans" type={3}>
+            Noto-Sans 3
+          </Typography>
         </View>
       </ImageBackground>
     </>
@@ -28,8 +50,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#f3f4f1",
-    opacity: 0.2,
+    backgroundColor: "rgba(243,244,241,0.2)",
     alignItems: "center",
     height: 470,
     width: "95%",
@@ -37,8 +58,11 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   logo: {
-    height: 100,
-    width: 100,
+    marginTop: 100,
+    position: "absolute",
+    height: 160,
+    width: 105,
+    opacity: 0.2,
   },
   background: {
     width: "100%",
